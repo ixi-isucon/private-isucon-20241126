0db06cc2cfb2b854fa9b496d1d13c7d4439fbfc2
gin <10017674+lirlia@users.noreply.github.com>
2024-11-26 00:04:47 +0900

    add hostname and profiles

---
+++ ansible/roles/common/files/1001_aliases.sh
@@ -0,0 +1,23 @@
+# Description: Aliases for the shell
+
+# shortcuts
+alias cd-log="cd /var/log"
+alias cd-log-nginx="cd /var/log/nginx"
+alias cd-log-mysql="cd /var/log/mysql"
+
+alias cd-mysql="cd /etc/mysql"
+alias cd-mysql-conf="cd /etc/mysql/conf.d"
+
+alias cd-nginx="cd /etc/nginx"
+alias cd-nginx-sites="cd /etc/nginx/sites-available"
+
+alias cd-systemd="cd /etc/systemd/system"
+
+# alias
+alias ll="ls -l"
+alias la="ls -la"
+
+alias sys-restart="systemctl restart"
+alias sys-start="systemctl start"
+alias sys-status="systemctl status"
+alias sys-stop="systemctl stop"
